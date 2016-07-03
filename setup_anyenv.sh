19ae7ddcc3f00f49e4d978ea83e7da5ae271b265
Ryoma Kai <legknmj@rkai.jp>
2016-07-03 16:58:16 +0900

    add anyenv scripts

---
+++ setup_anyenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+anyenv install exenv
+anyenv install goenv
+anyenv install jenv
+anyenv install ndenv
+anyenv install phpenv
+anyenv install pyenv
+anyenv install rbenv
+anyenv install swiftenv

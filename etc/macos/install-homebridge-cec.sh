940c8aa2a46680329587379bbc3d666e5bb64f2d
Ryoma Kai <legknmj@icloud.com>
2018-12-16 11:17:08 +0900

    change to cmdswitch2

--- etc/macos/install-homebridge-cec.sh
+++ etc/macos/install-homebridge-cec.sh
@@ -7,31 +7,20 @@ ndenv rehash
 
 echo "#### Install homebridge..."
 npm -g install homebridge --unsafe-perm
+npm -g install homebridge-cmdswitch2
 exec $SHELL -l
 
-# install fix branch versions homebridge-cec-accessory
-cd ~/.anyenv/envs/ndenv/versions/v10.14.2/lib/node_modules
-git clone https://github.com/jbree/homebridge-cec-accessory.git
-cd homebridge-cec-accessory
-git checkout -b branch-timeout-fix origin/branch-timeout-fix
-npm install
-cd -
-
 # generate config.json and plist
-cp ~/Downloads/mac-homebridge.envrc $DOT/etc/macos/homebridge/.envrc
+cp ~/Downloads/mac-bridge.envrc $DOT/etc/macos/homebridge/.envrc
 cd $DOT/etc/macos/homebridge
 direnv allow
 mkdir -p ~/.homebridge
 erb config.json.erb > ~/.homebridge/config.json
 erb com.homebridge.server.plist.erb > ~/Library/LaunchAgents/com.homebridge.server.plist
-erb com.cecclient.server.plist.erb > ~/Library/LaunchAgents/com.cecclient.server.plist
 
 chmod 700 ~/Library/LaunchAgents
-chmod 600 ~/Library/LaunchAgents/com.cecclient.server.plist ~/Library/LaunchAgents/com.homebridge.server.plist
+chmod 600 ~/Library/LaunchAgents/com.homebridge.server.plist
 
 # load
-launchctl load ~/Library/LaunchAgents/com.cecclient.server.plist
-launchctl start com.cecclient.server
-
 launchctl load ~/Library/LaunchAgents/com.homebridge.server.plist
 launchctl start com.homebridge.server

96cf848d9665ffb833a38d7d53756907f0f76737
Ryoma Kai <legknmj@icloud.com>
2019-02-16 21:25:39 +0900

    Delete setup_anyenv.sh

--- etc/setup_anyenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-anyenv install exenv
-anyenv install jenv
-anyenv install ndenv
-anyenv install phpenv
-anyenv install pyenv
-anyenv install rbenv
-anyenv install swiftenv
-exec $SHELL -l

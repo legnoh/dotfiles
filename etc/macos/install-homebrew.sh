93744aa260b68baba313bb7bf341aa13803f3c4c
Ryoma Kai <legknmj@icloud.com>
2019-02-16 21:43:28 +0900

    add install-homebrew

---
+++ etc/macos/install-homebrew.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect
+
+set PW [lindex $argv 0]
+set Prompt "\[#$%>\]"
+
+set timeout 86400
+
+spawn ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+expect {
+    "Press RETURN to continue or any other key to abort" {
+        send "\n"
+    }
+    "Password:" {
+        send "${PW}\n"
+    }
+}

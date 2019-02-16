0c81fe3868ab1efe701bfac610b994b505b550e7
Ryoma Kai <legknmj@icloud.com>
2019-02-16 22:23:32 +0900

    Update install-cask.sh

--- etc/macos/install-cask.sh
+++ etc/macos/install-cask.sh
@@ -6,8 +6,5 @@ set Prompt "\[#$%>\]"
 set timeout 100000000
 
 spawn brew bundle --file=~/code/src/github.com/legnoh/dotfiles/pkg/Brewfile.cask
-expect {
-    "Password:" {
-        send "${PW}\n"
-    }
-}
+expect "Password:"
+send "${PW}\n"

a58600d8ac04aac2f843dbf4c4d4129c16b4b915
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2025-01-06 09:59:03 +0900

    update vscode extention

--- etc/macos/setup.sh
+++ etc/macos/setup.sh
@@ -52,17 +52,24 @@ ln -fs ${DOT}/pkg/vsc-settings.json ${VSCODE_CONF_DIR}/settings.json
 ln -fs ${DOT}/pkg/vsc-locale.json ${VSCODE_CONF_DIR}/locale.json
 
 ## VSCode plugins
+code --install-extension astro-build.astro-vscode
 code --install-extension cocopon.iceberg-theme
+code --install-extension github.vscode-github-actions
 code --install-extension github.vscode-pull-request-github
 code --install-extension golang.go
+code --install-extension hashicorp.terraform
+code --install-extension hrst.yamaha-rt
+code --install-extension hrst.yamaha-wlx
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-ceintl.vscode-language-pack-ja
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 code --install-extension ms-python.python
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension redhat.vscode-yaml
+code --install-extension sswg.swift-lang
 code --install-extension tamasfe.even-better-toml
 code --install-extension tyriar.sort-lines
+code --install-extension unifiedjs.vscode-mdx
 code --install-extension visualstudioexptteam.vscodeintellicode
 code --install-extension vscode-icons-team.vscode-icons
 
